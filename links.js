e3be6e3606ba2eb4637c8255ba321629353fe2d3
Tiri <tk1971@nyu.edu>
2026-01-12 16:32:05 -0500

    first initialized
    first initialized

---
+++ links.js
@@ -0,0 +1,34 @@
+const links = [
+    {
+        title: "Syllabus",
+        url: "https://tiri.notion.site/Software-for-People-Spring-26-2e33700074c68005b41ace3a339392e5?source=copy_link",
+        description: "Course syllabus"
+    },
+    {
+        title: "Symposium",
+        url: "https://yaleedu-my.sharepoint.com/:w:/g/personal/tiriree_kananuruk_yale_edu/IQCAyhHEgh7dTYAaY-ilykOAASRZyr2j3kLoS8GCjtOHs5A?e=vTASZD",
+        description: "Student symposium"
+    }
+];
+
+const semesterLinks = {
+    spring26: "https://tiri.notion.site/Software-for-People-Spring-26-2e33700074c68005b41ace3a339392e5?source=copy_link", 
+    fall25: "https://tiriree.github.io/S4P-F25/",
+    spring25: "https://tiriree.github.io/S4P25/"
+};
+marquee="Welcome to the class!!!"
+
+const people = [
+    { name: "Tiri", url: "https://example.com/alice" },
+    { name: "Fiorella", url: "https://example.com/bob" },
+    { name: "Le", url: "https://example.com/carol" },
+    { name: "Nakaya", url: "https://example.com/david" },
+    { name: "Sama", url: "https://example.com/eva" },
+    { name: "Jiangyue", url: "https://example.com/frank" },
+    { name: "Yuchen", url: "https://example.com/grace" },
+    { name: "Mingge", url: "https://example.com/henry" },
+    { name: "aaa", url: "https://example.com/iris" },
+    { name: "bbb", url: "https://example.com/jack" },
+    { name: "ccc", url: "https://example.com/kate" }
+];
+ 
